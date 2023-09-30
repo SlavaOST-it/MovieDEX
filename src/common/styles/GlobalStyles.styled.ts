12e34668026beaacfd3b93f2fe49f50127e39f3f
SlavaOST <slavaost-it@mail.ru>
2023-09-30 21:41:29 +0300

    add styles

--- src/common/styles/GlobalStyles.styled.ts
+++ src/common/styles/GlobalStyles.styled.ts
@@ -1,6 +1,6 @@
 import {createGlobalStyle} from "styled-components";
 
-export const GlobalStyled = createGlobalStyle<{ theme: any}>`
+export const GlobalStyled = createGlobalStyle<{ theme: any }>`
   *,
   *::before,
   *::after {
@@ -13,4 +13,32 @@ export const GlobalStyled = createGlobalStyle<{ theme: any}>`
     width: 10px;
     background-color: #565656;
   }
+
+  *::-webkit-scrollbar-thumb {
+    background-color: ${props => props.theme.colors.accent};
+    border-radius: 6px;
+  }
+
+  a, a:visited, a:hover {
+    text-decoration: none;
+    color: ${props => props.theme.colors.primary};
+  }
+
+  ul li {
+    list-style: none;
+  }
+
+  p {
+    color: ${props => props.theme.colors.secondary};
+  }
+
+  body {
+    background-color: ${props => props.theme.colors.background};
+
+    color: ${props => props.theme.colors.primary};
+    font-family: 'Inter', sans-serif;
+    font-size: 14px;
+    line-height: 1.7;
+  }
 `
+
--- src/common/styles/Theme.styled.ts
+++ src/common/styles/Theme.styled.ts
@@ -1,7 +1,23 @@
+const desktop = "(max-width: 1024px)"
+const tablet = "(max-width: 768px)"
+const mobile = "(max-width: 576px)"
+
 export const lightTheme = {
+    colors: {
+        background: "#ffffff",
 
+        primary: "#000000",
+        secondary: "#969696",
+        accent: "#da0018",
+    },
+
+    media: {
+        desktop: desktop,
+        tablet: tablet,
+        mobile: mobile,
+    }
 }
 
-export const darkTheme = {
+export const darkTheme = {}
 
-}
+

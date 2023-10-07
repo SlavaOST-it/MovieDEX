3f6f16915580a09b96a7c0111d556be20edadd76
SlavaOST <slavaost-it@mail.ru>
2023-10-07 20:06:24 +0300

    add

--- src/features/header/index.ts
+++ src/features/header/index.ts
@@ -1,6 +1,10 @@
 import * as premieresSelectors from "./selectors"
+import {ToggleThemeBtn} from "./toggleThemeBtn/ToggleThemeBtn";
+import {Logo} from "./logo/Logo";
 
 
 export {
-    premieresSelectors
+    premieresSelectors,
+    ToggleThemeBtn,
+    Logo
 }

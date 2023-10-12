33212ed552c643436d625fd3ca645c2b96d26660
SlavaOST <slavaost-it@mail.ru>
2023-10-12 17:57:37 +0300

    add routes

--- src/utils/routes/routes.ts
+++ src/utils/routes/routes.ts
@@ -2,5 +2,7 @@ export const PATH = {
     startPage: '/startPage',
     topFilms: '/topFilms',
     premieres: '/premieres',
+    films: '/films',
+    categories: '/categoriesReducer',
     page404: '/404',
 }

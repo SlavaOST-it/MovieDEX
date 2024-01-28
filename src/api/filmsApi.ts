2f0758b9a403d0e067c4f982fe10a86b8fadadb4
SlavaOST <slavaost-it@mail.ru>
2024-01-28 21:38:55 +0300

    feat: add response

--- src/api/filmsApi.ts
+++ src/api/filmsApi.ts
@@ -2,6 +2,8 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 import {PremieresFilmsType} from "./types/PremieresFilmsType";
 import {FilmsTypes} from "./types/FilmsTypes";
 
+import {CategoriesType} from "./types/CategoriesTypes";
+
 
 export const filmsAPI = createApi({
     reducerPath: "filmsAPI",
@@ -14,14 +16,6 @@ export const filmsAPI = createApi({
         }
     }),
     endpoints: (build) => ({
-        getTopFilms: build.query<FilmsTypes, number>({
-            query: (currentPage: number) => {
-                return {
-                    url: `films/collections?type=TOP_250_MOVIES&page=${currentPage}`,
-                }
-            }
-        }),
-
         getPremieresFilms: build.query<PremieresFilmsType, { year: number, month: string }>({
             query: ({year, month}) => {
                 return {
@@ -35,11 +29,46 @@ export const filmsAPI = createApi({
             },
         }),
 
+        getTopFilms: build.query<FilmsTypes, number>({
+            query: (currentPage: number) => {
+                return {
+                    url: `films/collections?type=TOP_250_MOVIES&page=${currentPage}`,
+                }
+            }
+        }),
 
-    })
+        getSerials: build.query<FilmsTypes, any>({
+            query: (payload) => {
+                return {
+                    method: 'GET',
+                    url: `films?order=RATING&type=TV_SERIES&ratingFrom=5&ratingTo=10&yearFrom=1000&yearTo=2024&page=1`,
+                    params: {
+                        payload
+                    }
+                }
+            }
+        }),
+
+        getCategories: build.query<CategoriesType, {}>({
+            query() {
+                return {
+                    method: 'GET',
+                    url: `films/filters`,
+                }
+            }
+        }),
+
+
+
+        // MINI_SERIES
+        // TV_SERIES
+    }),
+
 });
 
 export const {
     useGetTopFilmsQuery,
     useGetPremieresFilmsQuery,
+    useGetSerialsQuery,
+    useGetCategoriesQuery,
 } = filmsAPI

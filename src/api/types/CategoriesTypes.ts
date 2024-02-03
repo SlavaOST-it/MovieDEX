59574f31c332bee14a01584c4c42b3911527aa0f
SlavaOST <slavaost-it@mail.ru>
2024-02-03 21:43:17 +0300

    fix: fix types

--- src/api/types/CategoriesTypes.ts
+++ src/api/types/CategoriesTypes.ts
@@ -1,30 +1,43 @@
-export type GenresType = {
+export type GenreType = {
     id: number,
     genre: string
 }
 
-export type CountriesType = {
+export type CountryType = {
     id: number,
     country: string
 }
 
 export type CategoriesType = {
-    genres: GenresType [],
-    countries: CountriesType []
+    genres: GenreType [],
+    countries: CountryType []
 }
 
 export type OrderType = 'RATING' | 'NUM_VOTE' | 'YEAR'
 export type StyleFilmType = 'FILM' | 'TV_SHOW' | 'TV_SERIES' | 'MINI_SERIES' | 'ALL'
 
-export type SearchType = {
-    order: OrderType,
-    type: StyleFilmType,
+export type SearchTypeOLD = {
+    order?: OrderType,
+    type?: StyleFilmType,
+    ratingFrom?: number,
+    ratingTo?: number,
+    yearFrom?: number,
+    yearTo?: number,
+    keyword?: string,
+    currentPage?: number,
     currentGenreId?: number,
     currentCountryId?: number,
-    ratingFrom: number,
-    ratingTo: number,
-    yearFrom: number,
-    yearTo: number,
-    keyword: string,
-    currentPage: number,
+}
+
+export type SearchType = {
+    countries?: number,
+    genres?: number,
+    order?: OrderType,
+    type?: StyleFilmType,
+    ratingFrom?: number,
+    ratingTo?: number,
+    yearFrom?: number,
+    yearTo?: number,
+    keyword?: string,
+    page?: number,
 }

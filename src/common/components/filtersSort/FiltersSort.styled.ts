4b797274cda77b76d88fbeebc3f6049618f39985
SlavaOST <slavaost-it@mail.ru>
2024-02-22 17:49:14 +0300

    feat: add style for FiltersSort.tsx

---
+++ src/common/components/filtersSort/FiltersSort.styled.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import {SelectValue} from "../sortValue/CustomSelectValue";
+
+
+export const ResetButton = styled(SelectValue)`
+`
+
+export const SortItem = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+`
+export const FiltersBlock = styled.div`
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    width: 100%;
+    padding-bottom: 30px;
+    margin-bottom: 30px;
+    border-bottom: 1px solid rgb(57, 78, 90);
+    gap: 20px;
+`

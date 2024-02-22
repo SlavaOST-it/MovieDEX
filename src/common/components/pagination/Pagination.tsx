b202f56a681181ec30c3cf5e5ad7185070914f4d
SlavaOST <slavaost-it@mail.ru>
2024-02-22 17:48:29 +0300

    feat: add jump to the last page

--- src/common/components/pagination/Pagination.tsx
+++ src/common/components/pagination/Pagination.tsx
@@ -12,66 +12,76 @@ type PaginationType = {
 
 export const Pagination: FC<PaginationType> = ({
                                                    totalItemsCount,
-                                                   pageSize= 1,
+                                                   pageSize = 1,
                                                    onPageChanges,
                                                    currentPage,
                                                    portionSize = 5
                                                }) => {
 
-        const pagesCount = Math.ceil(totalItemsCount / pageSize);
-        const pages = []
-        for (let i = 1; i <= pagesCount; i++) {
-            pages.push(i)
-        }
-
-        const portionCount = Math.ceil(pagesCount / portionSize);
-
-        const [portionNumber, setPortionNumber] = useState(1);
-
-        useEffect(() => {
-            setPortionNumber(1)
-        }, [])
-
-        const leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
-        const rightPortionPageNumber = (portionNumber * portionSize);
-
-        return (
-            <PaginationWrapper>
-                {portionNumber > 1 &&
-                    <NextPrevPage
-                        onClick={() => {
-                            setPortionNumber(portionNumber - 1)
-                        }}
-                    >
-                        {'<<'}
-                    </NextPrevPage>}
-
-                {pages
-                    .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
-                    .map(p => {
-                        return <CurrentPage $selected_page={currentPage === p} key={p}
-                                            onClick={() => {
-                                                onPageChanges(p)
-                                            }}
-                        >
-                            {p}
-                        </CurrentPage>
-                    })}
-
-                {totalItemsCount > 0 && !currentPage &&
-                    <>
-                        <span>...</span>
-                        <CurrentPage $selected_page={false}>{totalItemsCount}</CurrentPage>
-                    </>}
-
-                {portionCount > portionNumber &&
-                    <NextPrevPage
-                        onClick={() => {
-                            setPortionNumber(portionNumber + 1)
-                        }}
-                    >
-                        {'>>'}
-                    </NextPrevPage>}
-            </PaginationWrapper>
-        );
+    const pagesCount = Math.ceil(totalItemsCount / pageSize);
+    const pages = []
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i)
     }
+
+    const portionCount = Math.ceil(pagesCount / portionSize);
+
+    const [portionNumber, setPortionNumber] = useState(1);
+
+    useEffect(() => {
+        setPortionNumber(1)
+    }, [totalItemsCount])
+
+    const leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+    const rightPortionPageNumber = (portionNumber * portionSize);
+
+
+    const setLastPortionHandler = () => {
+        setPortionNumber(portionCount)
+        onPageChanges(pagesCount)
+    }
+
+    return (
+        <PaginationWrapper>
+            {portionNumber > 1 &&
+                <NextPrevPage
+                    onClick={() => {
+                        setPortionNumber(portionNumber - 1)
+                    }}
+                >
+                    {'<<'}
+                </NextPrevPage>}
+
+            {pages
+                .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
+                .map(p => {
+                    return <CurrentPage $selected_page={currentPage === p} key={p}
+                                        onClick={() => {
+                                            onPageChanges(p)
+                                        }}
+                    >
+                        {p}
+                    </CurrentPage>
+                })}
+
+            {(pagesCount > 0) && (Math.ceil(pagesCount / portionSize) !== portionNumber) &&
+                <>
+                    <span>...</span>
+                    <CurrentPage $selected_page={false}
+                                 onClick={setLastPortionHandler}>
+                        {totalItemsCount}
+                    </CurrentPage>
+                </>
+            }
+
+            {portionCount > portionNumber &&
+                <NextPrevPage
+                    onClick={() => {
+                        setPortionNumber(portionNumber + 1)
+                    }}
+                >
+                    {'>>'}
+                </NextPrevPage>}
+        </PaginationWrapper>
+    );
+}

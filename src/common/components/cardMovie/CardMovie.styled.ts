22034f51f807cd3ef6a5866d1373676e33f31166
SlavaOST <slavaost-it@mail.ru>
2024-02-24 19:59:21 +0300

    fix: took the styles out of the component

--- src/common/components/cardMovie/CardMovie.styled.ts
+++ src/common/components/cardMovie/CardMovie.styled.ts
@@ -1,1 +1,59 @@
 import styled from "styled-components";
+import { ThemeType } from "../../types/commonTypes";
+
+
+export const Description = styled.div<{ theme: ThemeType }>`
+    color: ${props => props.theme.colors.secondary};
+`
+
+export const Rating = styled.div<{ theme: ThemeType }>`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    width: 46px;
+    height: 46px;
+    border-radius: 50%;
+    background-color: #ec8802;
+
+    font-size: 18px;
+    font-weight: 700;
+
+    position: absolute;
+    top: 230px;
+    right: -20px;
+
+    box-shadow: ${props => props.theme.box_shadow};
+`
+
+export const ImageCard = styled.img <{ theme: ThemeType }>`
+    width: 200px;
+    height: 300px;
+
+    margin-bottom: 8px;
+    box-shadow: ${props => props.theme.box_shadow};
+    transition: ${props => props.theme.transition};
+`
+
+export const TitleCard = styled.h3`
+    font-family: Roboto;
+`
+
+export const CardStyle = styled.div<{ theme: ThemeType }>`
+    max-width: 200px;
+    max-height: 550px;
+
+    border-radius: 15px;
+
+    position: relative;
+    cursor: pointer;
+    transition: ${props => props.theme.transition};
+
+    &:hover {
+        ${ImageCard} {
+            box-shadow: 1px 8px 12px 10px rgba(0, 0, 0, 0.8);
+            transition: ${props => props.theme.transition};
+            transform: scale(1.04)
+        }
+    }
+`

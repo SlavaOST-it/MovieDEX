543a3ff9aa281d9f70de8f2c5987e977947cecac
SlavaOST <slavaost-it@mail.ru>
2024-02-24 20:00:20 +0300

    feat: add function set id movie

--- src/common/components/cardMovie/CardMovie.tsx
+++ src/common/components/cardMovie/CardMovie.tsx
@@ -1,85 +1,43 @@
 import React, {FC} from 'react';
-import styled from "styled-components";
-import {PremiereItemType} from "../../../api/types/PremieresFilmsType";
+import {NavLink} from "react-router-dom";
 import {FilmItemType} from "../../../api/types/FilmsTypes";
-import {ThemeType} from "../../types/commonTypes";
+import {PremiereItemType} from "../../../api/types/PremieresFilmsType";
+
+import {PATH} from "../../../utils/routes/routes";
+import {setIdMovie} from "../../../bll/reducers/movie/movieReducer";
+
+import {useAppDispatch} from "../../../utils/hooks/hooks";
+
+import {CardStyle, Description, ImageCard, Rating, TitleCard} from "./CardMovie.styled";
 
 
 type CardMovieType = {
     item: PremiereItemType | FilmItemType
 }
 
-export const CardMovie: FC<CardMovieType> = ({
-                                                 item
-                                             }) => {
+export const CardMovie: FC<CardMovieType> = ({item}) => {
+    const dispatch = useAppDispatch()
+
     const genre = item.genres.map((el, index) => <span key={index}>{el.genre + " "}</span>)
 
     let rating = 'ratingKinopoisk' in item ? item.ratingKinopoisk : null
 
+    const setIdMovieHandler = (id: number) => {
+        dispatch(setIdMovie({id}))
+    }
 
     return (
         <CardStyle>
-            <ImageCard src={item.posterUrlPreview} alt={''}/>
-            {rating && <Rating>{rating}</Rating>}
-            <TitleCard>{item.nameRu}</TitleCard>
-            <Description>{genre}</Description>
-            <Description>{item.year}</Description>
+            <NavLink
+                to={PATH.movie}
+                onClick={() => setIdMovieHandler(item.kinopoiskId)}
+            >
+                <ImageCard src={item.posterUrlPreview} alt={''}/>
+                {rating && <Rating>{rating}</Rating>}
+                <TitleCard>{item.nameRu}</TitleCard>
+                <Description>{genre}</Description>
+                <Description>{item.year}</Description>
+            </NavLink>
         </CardStyle>
     );
 };
-
-
-export const Description = styled.div<{ theme: ThemeType }>`
-    color: ${props => props.theme.colors.secondary};
-`
-export const Rating = styled.div<{ theme: ThemeType }>`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    width: 46px;
-    height: 46px;
-    border-radius: 50%;
-    background-color: #ec8802;
-
-    font-size: 18px;
-    font-weight: 700;
-
-    position: absolute;
-    top: 230px;
-    right: -20px;
-
-    box-shadow: ${props => props.theme.box_shadow};
-`
-
-export const ImageCard = styled.img <{ theme: ThemeType }>`
-    width: 200px;
-    height: 300px;
-
-    margin-bottom: 8px;
-    box-shadow: ${props => props.theme.box_shadow};
-    transition: ${props => props.theme.transition};
-`
-
-export const TitleCard = styled.h3`
-    font-family: Roboto;
-`
-
-export const CardStyle = styled.div<{ theme: ThemeType }>`
-    max-width: 200px;
-    max-height: 550px;
-
-    border-radius: 15px;
-
-    position: relative;
-    cursor: pointer;
-    transition: ${props => props.theme.transition};
-
-    &:hover {
-        ${ImageCard} {
-            box-shadow: 1px 8px 12px 10px rgba(0, 0, 0, 0.8);
-            transition: ${props => props.theme.transition};
-            transform: scale(1.04)
-        }
-    }
-`

7caca8e9fa201987231825267c7f0204195e5290
SlavaOST <slavaost-it@mail.ru>
2024-02-29 20:37:09 +0300

    feat: add universal component

---
+++ src/common/components/backButton/BackButton.styled.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+
+export const BackButton = styled.button`
+    font-family: 'Jura', sans-serif;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 1.7;
+`
